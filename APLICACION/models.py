3adf3a6ba7446c1b61bad8a6b8d041f075bd9a27
LeoChN <leonardo.chazarro.n@gmail.com>
2022-11-24 23:24:24 -0400

    dev: modificacion models

--- APLICACION/models.py
+++ APLICACION/models.py
@@ -31,11 +31,12 @@ class funcionarios (models.Model):
     def __str__(self):
         return self.nombre_fun 
     
+    
 
 class mascota (models.Model):
   nombre_masc = models.CharField(max_length=45)
-  tipo = models.ForeignKey(tipo_m,related_name='%(class)s_requests_created')
-  raza = models.ForeignKey(raza,related_name='%(class)s_requests_created')
+  tipo = models.ForeignKey(tipo_m,on_delete=models.CASCADE)
+  raza = models.ForeignKey(raza,on_delete=models.CASCADE)
   def __str__(self):
       return self.nombre_masc 
 
@@ -44,7 +45,7 @@ class clientes (models.Model):
     nombre_cli = models.CharField(max_length=45)
     direccion = models.CharField(max_length=45)
     fono = models.CharField(max_length=45)
-    nom_mas = models.ForeignKey(mascota,related_name='%(class)s_requests_created')
+    nom_mas = models.ForeignKey(mascota,on_delete=models.CASCADE)
     def __str__(self):
         return self.nombre_cli, self.nom_mas 
 
@@ -52,7 +53,7 @@ class clientes (models.Model):
 
 class cita(models.Model):
     fecha = models.DateField()
-    tipoat = models.ForeignKey(tipo_at,related_name='%(class)s_requests_created')
-    funcionario = models.ForeignKey(funcionarios,related_name='%(class)s_requests_created')
-    cliente = models.ForeignKey(clientes,related_name='%(class)s_requests_created')
-    mascota = models.ForeignKey(clientes,related_name='%(class)s_requests_created')
+    tipoat = models.ForeignKey(tipo_at,on_delete=models.CASCADE)
+    funcionario = models.ForeignKey(funcionarios,on_delete=models.CASCADE)
+    cliente = models.ForeignKey(clientes,related_name = 'elclientexd',on_delete=models.CASCADE)
+    mascota = models.ForeignKey(clientes,related_name = 'lamascotaxd',on_delete=models.CASCADE)
